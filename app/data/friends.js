ebca61c6cf02e18a8c41dcf8c52d669bda3ded26
Steven Sovak <sjs0509@gmail.com>
2018-09-11 15:14:07 -0500

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,82 +1,82 @@
 var friends = [
     {
-      "name":"",
-      "photo":"",
+      "name":"Elon Musk",
+      "photo":"https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2018/09/07/105438875-1536336503523screen-shot-2018-09-07-at-12.07.32-pm.530x298.jpg",
+      "scores":[
+          5,
+          2,
+          5,
+          1,
+          3,
+          5,
+          5,
+          1,
+          1,
+          3
+        ]
+    },
+    {
+      "name":"Hans Moleman",
+      "photo":"http://www.fourfingerdiscount.com.au/wp-content/uploads/2016/02/hans-moleman-dmv.jpg",
       "scores":[
           1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
+          5,
+          3,
+          2,
+          3,
+          3,
+          3,
+          4,
+          5,
           1
         ]
     },
     {
-      "name":"",
-      "photo":"",
+      "name":"Mr. Peanutbutter",
+      "photo":"https://vignette.wikia.nocookie.net/bojackhorseman/images/b/be/Mr_Peanutbutter_2007.jpeg",
       "scores":[
+          5,
+          1,
+          5,
+          1,
+          5,
+          4,
           1,
           1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1
+          5,
+          5
         ]
     },
     {
-      "name":"",
-      "photo":"",
+      "name":"LeBron James",
+      "photo":"https://www.gannett-cdn.com/-mm-/059699e753c5bf8a294b4060d295d1b3620b5ce5/c=359-0-3268-2187/local/-/media/2018/07/01/USATODAY/USATODAY/636660739455122847-USATSI-10602752.jpg",
       "scores":[
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1
+          3,
+          4,
+          5,
+          3,
+          2,
+          2,
+          3,
+          4,
+          3,
+          5
         ]
     },
     {
-      "name":"",
-      "photo":"",
+      "name":"Rick Sanchez",
+      "photo":"https://pbs.twimg.com/profile_images/870105716702445570/NGZ7ZMjw_400x400.jpg",
       "scores":[
+          4,
+          3,
+          2,
           1,
           1,
+          5,
+          5,
           1,
           1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1
-        ]
-    },
-    {
-      "name":"",
-      "photo":"",
-      "scores":[
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1
+          2
         ]
     }
   ];
